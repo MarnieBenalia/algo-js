92dacc259955d45f7d7d4f89e41ca5c09894d0fe
MarnieBenalia <marnie.benalia@gmail.com>
2022-11-24 17:03:59 +0100

    dossier

--- 02controle structures/2.4.js
+++ 02controle structures/2.4.js
@@ -4,11 +4,11 @@ while(i < 100){
 
 if (i%2==0){
 
-  console.log(i/=2);
+  console.log(i/2);
 
 } else if (i%2==1){
 
-    console.log(i*=3);
+    console.log(i*3);
 }
 
 alert(i);
--- 02controle structures/2.7.js
+++ 02controle structures/2.7.js
@@ -1,1 +1,3 @@
 let n = Number(prompt("enter a number"));
+//ca faut le mettre dans une boucle 
+Number (prompt("enter" + n +"number"))
