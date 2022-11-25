6910a6d14cd402ea63c9f2d94773a770c492ada9
Marnie Benalia <marnie.benalia@gmail.com>
2022-11-25 10:48:56 +0100

    dossier

--- 02controle structures/2.7.js
+++ 02controle structures/2.7.js
@@ -1,3 +1,12 @@
-let n = Number(prompt("enter a number"));
-//ca faut le mettre dans une boucle 
-Number (prompt("enter" + n +"number"))
+let x= Number(0)
+let n = Number(prompt("enter a number"))
+let somme = Number(0)
+
+while (n>x) { 
+    
+    x = Number(prompt("enter " + n + " numbers"))
+
+    somme+=x
+}
+
+alert(somme)
---
+++ 03arrays/3.0.js
@@ -0,0 +1,7 @@
+const learner = ["Arno Volts" ,"Aurélien Mariaule" ,"Aurore Lemaire" ,"Benjamin Porsont" ,
+"Céline Verreydt" ,"Corentin Miserque" ,"Dominique Coppée" ,"Edouard de Romrée de Vichenet" ,"Hugo Goorickx ","Jofrey Houyoux" ,
+"Jonathan Manes" ,"jonathan bajoux" ,"Laura Wilhelmi" ,"Lysie Soyez" ,"Marnie Benalia" ,"Mathilde Cornelis" ,"Milo Bonnet" ,
+"Nadim El Nakadi" ,"Nathalie Vanden Abeele" ,"Nathalie Goffette" ,"Nour Everaert" ,
+"Pierre Mauriello" ,"Quentin Bource ","Virginie Dourson"];
+
+learner.forEach(el => console.log(el))
