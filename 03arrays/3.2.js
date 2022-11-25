f5f2e03e6b3a362f1d1190bc16f1f368365b8a52
Marnie Benalia <marnie.benalia@gmail.com>
2022-11-25 13:37:55 +0100

    dossier

---
+++ 03arrays/3.2.js
@@ -0,0 +1,3 @@
+const num = [1,2,3,4,5]
+const result = num.reduce((a, b) => a + b) / num .length
+console.log(result)
---
+++ 03arrays/3.4.js
@@ -0,0 +1,4 @@
+const mylist = [1, 2, 3, 4, 5]
+
+console.log(Math.min(mylist))
+console.log(Math.max(mylist))
